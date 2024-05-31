ac24adddd1f47c1053463977b64bb2d801f23e1c
Agnes <agypeter97@gmail.com>
2024-06-01 01:05:11 +0300

    created templates folder

--- frontend/frontendApp/views.py
+++ frontend/frontendApp/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import requests
+import os
 
-# Create your views here.
-def index (request):
-    return render (request, "")
+RASA_SERVER_URL = "http://localhost:5005/webhooks/rest/webhook"
+
+def index(request):
+    return render(request, 'index.html')
+
+def chat(request):
+    if request.method == "POST":
+        user_message = request.POST.get('message')
+        response = requests.post(RASA_SERVER_URL, json={"message": user_message})
+        bot_response = response.json()
+        return JsonResponse({"response": bot_response})
